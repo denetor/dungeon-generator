a36e65f6226a9beae9ab919d7000f602511b8d2d
Nicola Da Rold <ndarold@pataviumenergia.it>
2025-04-17 11:20:01 +0200

    empty project

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
-import * as _ from 'lodash';
-import './style.css';
-import logo from './assets/logo.svg';
-
 function helloComponent() {
     const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    // img element, if i need to use an image
-    // const myLogo = new Image();
-    // myLogo.src = logo;
-    // element.appendChild(myLogo);
+    element.innerHTML = 'Hello world';
 
     return element;
 }
